3827557646634ec2158b13d58aedd533c84a5c8a
namnguyen315 <namnguyen3152001xtnd@gmail.com>
2024-10-02 22:42:29 +0700

    rename file faqModule.js

--- src/js/faqModule.js
+++ src/js/faqModule.js
@@ -1,19 +1,19 @@
 export function initFAQ() {
-  const faqItems = document.querySelectorAll(".faq-item");
-  console.log(faqItems);
-
-  faqItems.forEach(item => {
-    item.addEventListener("click", function() {
-      const answer = this.querySelector(".faq-answer");
-      toggleFAQ(answer);
+    const faqItems = document.querySelectorAll(".faq-item");
+    console.log(faqItems);
+  
+    faqItems.forEach(item => {
+      item.addEventListener("click", function() {
+        const answer = this.querySelector(".faq-answer");
+        toggleFAQ(answer);
+      });
     });
-  });
-}
-
-function toggleFAQ(answer) {
-  if (answer.style.display === "block") {
-    answer.style.display = "none";
-  } else {
-    answer.style.display = "block";
   }
-}
+  
+  function toggleFAQ(answer) {
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  }
